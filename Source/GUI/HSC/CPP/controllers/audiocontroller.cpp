5f2e3ff473a31ee94c8c4d376c7c202f9948f7ad
trieuhpn <vikingtc4@gmail.com>
2025-04-20 21:55:20 +0700

    HSC-4 link the music player and audio controller

--- Source/GUI/HSC/CPP/controllers/audiocontroller.cpp
+++ Source/GUI/HSC/CPP/controllers/audiocontroller.cpp
@@ -1,10 +1,18 @@
 #include "audiocontroller.h"
 
 audioController::audioController(QObject *parent)
-    : QObject(parent)
-    , m_volumeLevel(50)
+    : QObject(parent),
+      m_volumeLevel(0),
+      m_audioOutput(std::make_unique<QAudioOutput>())
 {
+    if (m_audioOutput)
+        m_audioOutput->setVolume(m_volumeLevel);
+}
 
+QAudioOutput* audioController::audioOutput() const
+{
+    qDebug() << "Volume output get";
+    return m_audioOutput.get();
 }
 
 int audioController::volumeLevel() const
@@ -15,25 +23,28 @@ int audioController::volumeLevel() const
 void audioController::increaseVolume(const int &val)
 {
     int newVolumeLevel {m_volumeLevel + val};
-    if ( 0 >= newVolumeLevel )
-    {
+
+    if (newVolumeLevel < 0)
         newVolumeLevel = 0;
-    }
-    if ( 100 <= newVolumeLevel )
-    {
+    else if (newVolumeLevel > 100)
         newVolumeLevel = 100;
-    }
+
     setVolumeLevel(newVolumeLevel);
-    qDebug() << "Volume increased" << newVolumeLevel;
-
+    qDebug() << "Volume increased to:" << newVolumeLevel;
 }
-
-
 
 void audioController::setVolumeLevel(int newVolumeLevel)
 {
     if (m_volumeLevel == newVolumeLevel)
         return;
+
     m_volumeLevel = newVolumeLevel;
+
+    if (m_audioOutput) {
+        qDebug() << "Volume increased to :" << m_volumeLevel;
+
+        m_audioOutput->setVolume(m_volumeLevel / 100.0);
+    }
+
     emit volumeLevelChanged();
 }
